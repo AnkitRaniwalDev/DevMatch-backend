061043440166e746484c0a43ca0f339d16d5fb7d
AnkitRaniwalDev <ankit.alw011@gmail.com>
2026-07-01 20:05:28 +0530

    fix: change Middleware folder and imports to lowercase

--- src/routes/bio.route.js
+++ src/routes/bio.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { bioController,getAllBios ,} from "../controllers/bio.controller.js";
-import { auth} from "../Middleware/auth.middleware.js";
+import { auth} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
